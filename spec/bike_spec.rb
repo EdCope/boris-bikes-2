e62613833bf8cb7bd8abd8175a988533c97f4922
Ed <ed321@hotmail.co.uk>
2022-01-26 14:43:39 +0000

    create bike object and tests

---
+++ spec/bike_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/bike.rb'
+
+describe Bike do
+  describe "#bike_works?" do
+    it "should respond to working" do
+      bike = Bike.new
+      expect(bike).to respond_to(:working?)
+    end
+  end
+end
