0b73149b04ba051cf42a8828347a91d9c6c01a6b
Ed <ed321@hotmail.co.uk>
2022-01-26 16:25:49 +0000

    Dock bike added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,24 @@
 require_relative './bike.rb'
 
 class DockingStation
+  attr_reader: @bikes
+  def initialize
+    @bikes = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bikes << bike
+  end
+
+  def station_status
+    if @bikes.length > 0
+      "Available"
+    else
+      "No Bikes"
+    end
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,4 @@
 require_relative "../lib/docking_station.rb"
-require_relative "../lib/bike.rb"
 
 describe DockingStation do 
   describe "#release_bike" do
@@ -9,10 +8,21 @@ describe DockingStation do
     # end
     it { should respond_to(:release_bike)}
 
+    it { expect(subject.release_bike).to be_working}
+    #it "should release a working bike" do
+      #expect(subject.release_bike).to be_working
+    #end
 
-    it "should release a working bike" do
+    #As a member of the public
+    #So I can return bikes I've hired
+    #I want to dock my bike at the docking station
+  end
+  describe "#dock_bike" do
+    it{ expect(subject).to respond_to(:dock_bike).with(1).argument}
+    it "should check the status of a station" do
       docking_station = DockingStation.new
-      expect(docking_station.release_bike).to respond_to(:working?)
+      bike = Bike.new
+      expect(subject.station_status).to eq("Available")
     end
   end
 end 
