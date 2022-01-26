d8a878c718442f0aba1ac7102abd15d4ebf322d7
Chris Middleton <ctjmiddleton@gmail.com>
2022-01-26 15:14:54 +0000

    Extra Docking Station Test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,9 +2,10 @@ require_relative '../lib/bike.rb'
 
 describe Bike do
   describe "#bike_works?" do
-    it "should respond to working" do
-      bike = Bike.new
-      expect(bike).to respond_to(:working?)
-    end
+    # it "should respond to working" do
+      # bike = Bike.new
+      # expect(bike).to respond_to(:working?)
+    #end
+    it { should respond_to(:working?)}
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require_relative "../lib/docking_station.rb"
+require_relative "../lib/bike.rb"
 
 describe DockingStation do 
   describe "#release_bike" do
@@ -7,5 +8,11 @@ describe DockingStation do
     #   expect(docking_station).to respond_to(:release_bike)
     # end
     it { should respond_to(:release_bike)}
+
+
+    it "should release a working bike" do
+      docking_station = DockingStation.new
+      expect(docking_station.release_bike).to respond_to(:working?)
+    end
   end
 end 
